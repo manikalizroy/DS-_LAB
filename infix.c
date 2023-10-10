b83b2d94750cbd6e5f432efc0f82db962557475c
manikalizroy <118897269+manikalizroy@users.noreply.github.com>
2023-10-11 01:25:03 +0530

    Update infix.c

--- infix.c
+++ infix.c
@@ -1,45 +1,168 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define size 25
 
-void push(char x)
+char s[25];
+int top = -1;
+
+void push(char item)
 {
-    stack[++top] = x;
+    if(top==size - 1)
+        printf("overflow\n");
+    else
+    {
+        top++;
+        s[top] = item;
+    }
+    
+char pop()
+{
+    if(top== -1)
+    {
+        printf("empty\n");
+      //  return '\0';
+    }    
+    else
+    {
+        int item;
+        item = s[top];
+        top -=1;
+        return item;
+    }
+}  
+
+int isp(char ch)
+{
+    switch (ch)
+    {
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': return 4;
+        case '^': return 5;
+        case '(': return 0;
+    }
+
 }
 
-void pop(char x)
-{
-    int store;
-    store = c[top];.
-    top -=1;
-}
-
-int precedence(char ch)
+int icp(char ch)
 {
     switch (ch)
     {
         case '+':
         case '-': return 1;
         case '*':
-        case '\': return 2;
-        case'^' : return 3;        
+        case '/': return 3;
+        case '^': return 6;
+        case '(': return 7;
+        case ')': return 0;
     }
-    return -1;
 }
 
-int len,i = 0;
-int findlen(char ch)
+int i = 0;
+//while(ch[i]!='\0')
 {
-    while(ch[i]!='\0')
-        len++;
+void infixToPostfix(char exp[],char post[])
+{
+    int postind = 0;
+    int i=0;
+    char item,op;
+    push('(');
+    //strcat(exp,')');
+    
+    while(exp[i]!= '\0')
+    {
+        item = exp[i];
+        op = s[top];
+        
+        if((item >='a'&& item <= 'z') || (item >='A'&& item <= 'Z') || (item >='0'&& item <= '9' ))
+        {
+            post[postind++] = item;
+         }
+        else if(item == '(')
+        {
+            push(item);
+        }
+        else if(item == ')')
+        {
+            while(op != '(')
+            {
+               post[postind++] = op;
+            }
+        pop();
+        }
+        
+        else
+        {
+            while(isp(item)>=isp(op))
+            {
+                op = pop();
+                post[postind++] = op;
+            }
+        push(item);    
+        }
+        i++;
+    }
+    
+    while(top >= 0 && s[top] != '(')
+    {
+        post[postind++] = pop();
+    }
+    post[postind] = '\0';
+    
 }
 
-
+int evaluate(char post[])
+{
+    char s[25];
+    int i = 0,top= -1;
+    
+    while(post[i]!='\0')
+    {
+        char item = post[i];
+        if(isdigit(item))
+        {
+            push(item - 0);
+        }
+        else if(isoperator(item))
+        {
+            int op2 = pop();
+            int op1 = pop();
+            switch (item)
+            {
+                case '+': push(op1 + op2);
+                        : break;
+                case '-': push(op1 - op2);
+                        : break;
+                case '*': push(op1 * op2);
+                        : break;
+                case '/': push(op1 / op2);
+                        : break;
+                case '^': push(op1 ^ op2);
+                        : break;
+            }
+        }i++;
+    }
+    
+    return pop();
+}
     
 
 int main()
 {
-    char exp[25] = "a*(b+c)*d";
+    char exp[25],post[25];
+    int result;
     
+    printf("enter infix exp: \n");
+    scanf("%[^\n]",exp);
     
+    infixToPostfix(exp,post);
+    printf("postfix exp is : %s\n",post);
     
+    result = evaluate(post);
+    printf("result: %d\n",post);
     
+    return 0;
 }
+
